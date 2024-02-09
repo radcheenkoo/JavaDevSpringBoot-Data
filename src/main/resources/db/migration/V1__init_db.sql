88cc44b0d207ebd4d2c5632c0d44586f220636c1
radcheenkoo <134805103+radcheenkoo@users.noreply.github.com>
2024-02-09 17:25:21 +0200

    h2

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,7 +1,7 @@
-CREATE TABLE note(
-id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-title VARCHAR(250) NOT NULL CHECK(LENGTH(title)>=3 AND LENGTH(title)<=250),
-content VARCHAR NOT NULL,
-last_updated_date DATE NOT NULL,
-created_date DATE NOT NULL
+CREATE TABLE note (
+    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    title VARCHAR(250) NOT NULL CHECK (LENGTH(title) >= 3 AND LENGTH(title) <= 250),
+    content VARCHAR NOT NULL,
+    last_updated_date DATE NOT NULL,
+    created_date DATE NOT NULL
 );
