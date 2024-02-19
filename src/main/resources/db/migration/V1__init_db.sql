67eb1981803de85fbf2c292a8abbe1ac768c8962
radcheenkoo <134805103+radcheenkoo@users.noreply.github.com>
2024-02-19 14:28:35 +0200

    migration

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -1,6 +1,7 @@
-CREATE TABLE users(
-id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-password VARCHAR(100) NOT NULL CHECK(LENGTH(password)>=2 AND LENGTH(password)<=100),
+CREATE TABLE note(
+id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+title VARCHAR(250) NOT NULL CHECK(LENGTH(title)>=3 AND LENGTH(title)<=250),
+content VARCHAR NOT NULL,
 last_updated_date DATE NOT NULL,
 created_date DATE NOT NULL
 );
